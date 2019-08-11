c5fbb6f17d55b285e4dea64d850180aaa50bdef9
(not) Sam Beck <notsambeck@gmail.com>
2019-08-11 16:41:32 -0700

    updated setup to read README.md

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     version='0.1',
     author='Sam Beck',
@@ -7,6 +10,7 @@ setup(
     name='pandabase',
     packages=['pandabase'],
     description="pandabase links pandas DataFrames to SQL databases. Supports read, append, and upsert.",
+    long_description = long_description,
     url="https://github.com/notsambeck/pandabase",
     classifiers=[
         "Programming Language :: Python :: 3",
